3ac8f2ebeaf5f889312cb5b9d4d2eedab664690e
Eddy <edward.gonzalez9708@gmail.com>
2018-12-13 20:14:16 -0500

    Installed eslint packages, added eslint extension to vs code, and fixed linter errors in React app/sever file.

--- src/components/recipes/RecipeForm.js
+++ src/components/recipes/RecipeForm.js
@@ -1,93 +1,108 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
-import "../../App.css";
-import { Redirect } from "react-router-dom";
+import React, { Component } from 'react'
+import axios from 'axios'
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
+import '../../App.css'
+import { Redirect } from 'react-router-dom'
 
 class RecipeForm extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
-      edit: props.edit,
-      recipe: props.recipe,
-      title: props.title,
-      description: props.description,
-      instructions: props.instructions,
-      ingredients: props.ingredients,
-      meal: props.meal,
-      image: props.image,
-      preptime: props.preptime,
+      edit: '',
+      recipe: '',
+      title: '',
+      description: '',
+      instructions: '',
+      ingredients: '',
+      meal: '',
+      image: '',
+      preptime: '',
       submitted: false
-    };
+      // Props are not being passed at the moment
+      // edit: props.edit,
+      // recipe: props.recipe,
+      // title: props.title,
+      // description: props.description,
+      // instructions: props.instructions,
+      // ingredients: props.ingredients,
+      // meal: props.meal,
+      // image: props.image,
+      // preptime: props.preptime,
+      // submitted: false
+    }
   }
 
   handleInputChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+    this.setState({ [e.target.name]: e.target.value })
+  }
 
-  componentDidMount() {
+  componentDidMount () {
     if (!this.state.edit) {
-      console.log("creating new recipe");
-      this.setState({ title: "", description: "", instructions: "", ingredients: "", meal: "", image: "", preptime: "" });
+      console.log('creating new recipe')
+      this.setState({ title: '', description: '', instructions: '', ingredients: '', meal: '', image: '', preptime: '' })
     } else {
-      console.log("updating recipe");
+      console.log('updating recipe')
     }
   }
 
   addRecipe = event => {
-    event.preventDefault();
-    const { ingredients, title, description, instructions, meal, image, preptime } = this.state;
-    const newRecipe = { ingredients, title, description, instructions, meal, image, preptime };
-    
+    event.preventDefault()
+    const { ingredients, title, description, instructions, meal, image, preptime } = this.state
+    const newRecipe = { ingredients, title, description, instructions, meal, image, preptime }
     if (this.state.edit) {
       const updatedRecipes = Object.assign({}, newRecipe, {
         id: this.state.recipe.id
-      });
-    axios
-      .put(`http://localhost:5555/recipes/${updatedRecipes.id}`, updatedRecipes)
-      .then(res => {
-        console.log(res.data);
-        this.setState({submitted: true});
       })
-      .catch(err => { 
-        console.log(err)
-      });
+      axios
+        .put(`http://localhost:5555/recipes/${updatedRecipes.id}`, updatedRecipes)
+        .then(() => {
+          this.setState({ submitted: true })
+        })
+        .catch(err => {
+          console.log(err)
+        })
     } else {
       axios
-        .post(`http://localhost:5555/recipes`, newRecipe)
-        .then (res => {
-          console.log(res.data);
-          this.setState({ title: "", description: "", meal: "", instructions: "", ingredients: "", image: "", preptime: "", submitted: true});
-          })
-          .catch(err => { console.log(err)
-          });
+        .post('http://localhost:5555/recipes', newRecipe)
+        .then(() => {
+          this.setState({
+            title: '',
+            description: '',
+            meal: '',
+            instructions: '',
+            ingredients: '',
+            image: '',
+            preptime: '',
+            submitted: true })
+        })
+        .catch(err => console.log(err))
     }
-  };
+  }
 
-  render() {
+  render () {
     return this.state.submitted ? (
-      <Redirect to ="/recipes" /> 
+      <Redirect to='/recipes' />
     ) : (
-      <div className="background">
-        <div className="form-container">
+      <div className='background'>
+        <div className='form-container'>
           <Form onSubmit={this.addRecipe}>
             <FormGroup>
               <Label>Title</Label>
               <Input
                 onChange={this.handleInputChange}
-                name="title"
-                type="text"
-                placeholder="Healthy eggs"
+                name='title'
+                type='text'
+                placeholder='Healthy eggs'
                 value={this.state.title}
               />
             </FormGroup>
             <FormGroup>
               <Label>Description</Label>
               <Input
-                type="text"
+                type='text'
                 onChange={this.handleInputChange}
-                name="description"
-                placeholder="Fried eggs with beans and spinach, topped with salsa and hot sauce"
+                name='description'
+                placeholder='Fried eggs with beans and spinach, topped with salsa and hot sauce'
                 value={this.state.description}
               />
             </FormGroup>
@@ -95,46 +110,46 @@ class RecipeForm extends Component {
               <Label>Meal</Label>
               <Input
                 onChange={this.handleInputChange}
-                name="meal"
-                type="text"
-                placeholder="Breakfast"
+                name='meal'
+                type='text'
+                placeholder='Breakfast'
                 value={this.state.meal}
               />
-              <FormText color="muted">Breakfast, lunch, or dinner.</FormText>
+              <FormText color='muted'>Breakfast, lunch, or dinner.</FormText>
             </FormGroup>
             <FormGroup>
               <Label>Ingredients</Label>
               <Input
-                type="text"
-                name="ingredients"
+                type='text'
+                name='ingredients'
                 onChange={this.handleInputChange}
                 value={this.state.ingredients}
-                placeholder="eggs, black beans, spinach, salsa, hot sauce"
+                placeholder='eggs, black beans, spinach, salsa, hot sauce'
               />
-              <FormText color="muted">
+              <FormText color='muted'>
                 Please list your ingredients seperated by a 'comma'.
               </FormText>
             </FormGroup>
             <FormGroup>
               <Label>Instructions</Label>
               <Input
-                type="textarea"
-                name="instructions"
+                type='textarea'
+                name='instructions'
                 onChange={this.handleInputChange}
                 value={this.state.instructions}
-                placeholder="Add oil and spinach to a frying pan. Once the spinach has started shrivelling up, break your desired number of eggs into the frying pan. Soon after, add beans. Once cooked, top with salsa and hot sauce."
+                placeholder='Add oil and spinach to a frying pan. Once the spinach has started shrivelling up, break your desired number of eggs into the frying pan. Soon after, add beans. Once cooked, top with salsa and hot sauce.'
               />
             </FormGroup>
             <FormGroup>
               <Label>Image</Label>
               <Input
                 onChange={this.handleInputChange}
-                name="image"
-                type="text"
-                placeholder="https://www.weightwatchers.com/images/1033/dynamic/foodandrecipes/2016/02/Southwest-InspiredBalckBeansAndEggs_JF16_EAT_FTR1_EGGS_800x800.jpg"
+                name='image'
+                type='text'
+                placeholder='https://www.weightwatchers.com/images/1033/dynamic/foodandrecipes/2016/02/Southwest-InspiredBalckBeansAndEggs_JF16_EAT_FTR1_EGGS_800x800.jpg'
                 value={this.state.image}
               />
-              <FormText color="muted">
+              <FormText color='muted'>
                 We recommend you find a square image that's at least 300x300
                 pixels.
               </FormText>
@@ -143,9 +158,9 @@ class RecipeForm extends Component {
               <Label>Preptime</Label>
               <Input
                 onChange={this.handleInputChange}
-                name="preptime"
-                type="text"
-                placeholder="15 minutes"
+                name='preptime'
+                type='text'
+                placeholder='15 minutes'
                 value={this.state.preptime}
               />
             </FormGroup>
@@ -153,8 +168,8 @@ class RecipeForm extends Component {
           </Form>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default RecipeForm;
+export default RecipeForm
--- src/components/recipes/SingleRecipe.js
+++ src/components/recipes/SingleRecipe.js
@@ -1,102 +1,102 @@
-import React, { Component } from "react";
-import "../../App.css";
-import { Alert, Button } from "reactstrap";
-import axios from "axios";
-import { Redirect, Link } from "react-router-dom";
-import Modal from "react-modal";
+import React, { Component } from 'react'
+import '../../App.css'
+import { Alert, Button } from 'reactstrap'
+import axios from 'axios'
+import { Redirect, Link } from 'react-router-dom'
+import Modal from 'react-modal'
 
 const customStyles = {
   content: {
-    top: "50%",
-    left: "50%",
-    right: "auto",
-    bottom: "auto",
-    marginRight: "-50%",
-    transform: "translate(-50%, -50%)"
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
   }
-};
+}
 
 class SingleRecipe extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       deleted: false,
       modalIsOpen: false
-    };
+    }
   }
 
   openModal =() => {
-    this.setState({ modalIsOpen: true });
+    this.setState({ modalIsOpen: true })
   }
 
   afterOpenModal =() => {
     // references are now sync'd and can be accessed.
-    this.subtitle.style.color = "#00";
+    this.subtitle.style.color = '#00'
   }
 
   closeModal =() => {
-    this.setState({ modalIsOpen: false });
+    this.setState({ modalIsOpen: false })
   }
 
-  componentDidMount() {
-    const id = this.props.match.params.id;
-    axios
-      .get(`http://localhost:5555/recipes/${id}`)
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  componentDidMount () {
+    // id is undefined so I commented the code below
+    // const id = this.props.match.params.id
+    // axios
+    //   .get(`http://localhost:5555/recipes/${id}`)
+    //   .then(res => {
+    //     console.log(res.data)
+    //   })
+    //   .catch(err => {
+    //     console.log(err)
+    //   })
   }
 
   deleteRecipe = () => {
     axios
       .delete(`http://localhost:5555/recipes/${this.state.recipe.id}`)
       .then(response => {
-        console.log("Deleting!", response);
-        this.setState({ deleted: true });
+        console.log('Deleting!', response)
+        this.setState({ deleted: true })
       })
-      .catch(error => console.log(error));
-  };
+      .catch(error => console.log(error))
+  }
 
-  render() {
+  render () {
     return this.state.deleted ? (
-      <Redirect to="/recipes" />
+      <Redirect to='/recipes' />
     ) : (
       <div>
-        <div className="recipe-card">
-          <div className="flex-wrapper">
+        <div className='recipe-card'>
+          <div className='flex-wrapper'>
             <div>
               <img
-                src="https://www.weightwatchers.com/images/1033/dynamic/foodandrecipes/2016/02/Southwest-InspiredBalckBeansAndEggs_JF16_EAT_FTR1_EGGS_800x800.jpg"
-                alt="Healthy eggs"
-                className="recipe-image"
+                src='https://www.weightwatchers.com/images/1033/dynamic/foodandrecipes/2016/02/Southwest-InspiredBalckBeansAndEggs_JF16_EAT_FTR1_EGGS_800x800.jpg'
+                alt='Healthy eggs'
+                className='recipe-image'
               />
             </div>
             <div>
-              <div className="delete-flex">
+              <div className='delete-flex'>
                 <h3>Healthy eggs</h3>
                 <div>
-                  <i class="far fa-edit fa-2x"></i>
+                  <i className='far fa-edit fa-2x' />
                   <i
-                    class="far fa-trash-alt fa-2x delete-icon"
-                    onClick={this.openModal}
-                  />
+                    className='far fa-trash-alt fa-2x delete-icon'
+                    onClick={this.openModal} />
                 </div>
               </div>
               <div>
-                <p className="recipe-description">Fried eggs with beans and spinach, topped with salsa and hot sauce</p>
+                <p className='recipe-description'>Fried eggs with beans and spinach, topped with salsa and hot sauce</p>
                 <h5>Instructions</h5>
-                <p className="recipe-instructions">Add oil and spinach to a frying pan. Once the spinach has started shrivelling up, break your desired number of eggs into the frying pan. Soon after, add beans. Once cooked, top with salsa and hot sauce.</p>
+                <p className='recipe-instructions'>Add oil and spinach to a frying pan. Once the spinach has started shrivelling up, break your desired number of eggs into the frying pan. Soon after, add beans. Once cooked, top with salsa and hot sauce.</p>
                 <h5>Ingredients</h5>
-                <p className="recipe-ingredients">eggs, black beans, spinach, salsa, hot sauce</p>
+                <p className='recipe-ingredients'>eggs, black beans, spinach, salsa, hot sauce</p>
                 <h5>Preptime</h5>
-                <p className="recipe-preptime">15 minutes</p>
+                <p className='recipe-preptime'>15 minutes</p>
               </div>
             </div>
           </div>
-          <Alert color="info">
+          <Alert color='info'>
             This meal is intended for <strong>breakfast</strong> but
             rules are meant to be broken.
           </Alert>
@@ -105,14 +105,14 @@ class SingleRecipe extends Component {
             onAfterOpen={this.afterOpenModal}
             onRequestClose={this.closeModal}
             style={customStyles}
-            contentLabel="Example Modal"
+            contentLabel='Example Modal'
           >
             <div>
               <h4 ref={subtitle => (this.subtitle = subtitle)}>
-                Are you sure you want to delete the <br/> Healthy eggs?
+                Are you sure you want to delete the <br /> Healthy eggs?
               </h4>
-              <div className="modal-buttons">
-                <Button color="danger" onClick={this.deleteRecipe} className="margin-right">
+              <div className='modal-buttons'>
+                <Button color='danger' onClick={this.deleteRecipe} className='margin-right'>
                   Yes, delete this recipe.
                 </Button>
                 <Button onClick={this.closeModal}>No, keep it.</Button>
@@ -120,12 +120,12 @@ class SingleRecipe extends Component {
             </div>
           </Modal>
         </div>
-        <Link to="/recipes">
-          <div className="searchbar">View all the recipes...</div>
+        <Link to='/recipes'>
+          <div className='searchbar'>View all the recipes...</div>
         </Link>
       </div>
-    );
+    )
   }
 }
 
-export default SingleRecipe;
+export default SingleRecipe
