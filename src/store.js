da5e5eb1fdf39289b9b99715a4daf6ce38b08eda
Blake Fletcher <blkfltchr@gmail.com>
2018-12-16 22:52:25 -0500

    Fixed store.js to comply with eslint

--- src/store.js
+++ src/store.js
@@ -1,23 +1,23 @@
-import {createStore, combineReducers, compose} from 'redux' 
+import { createStore, combineReducers, compose } from 'redux'
 import firebase from 'firebase'
 import 'firebase/firestore'
-import {reactReduxFirebase, firebaseReducer} from 'react-redux-firebase'
-import {reduxFirestore, firestoreReducer} from 'redux-firestore'
+import { reactReduxFirebase, firebaseReducer } from 'react-redux-firebase'
+import { reduxFirestore, firestoreReducer } from 'redux-firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBaayrHhgqKEVKFOMh-z0QFgPrqC3lZoW4",
-    authDomain: "gotorecipes-b3714.firebaseapp.com",
-    databaseURL: "https://gotorecipes-b3714.firebaseio.com",
-    projectId: "gotorecipes-b3714",
-    storageBucket: "gotorecipes-b3714.appspot.com",
-    messagingSenderId: "715471095704"
-};
+  apiKey: 'AIzaSyBaayrHhgqKEVKFOMh-z0QFgPrqC3lZoW4',
+  authDomain: 'gotorecipes-b3714.firebaseapp.com',
+  databaseURL: 'https://gotorecipes-b3714.firebaseio.com',
+  projectId: 'gotorecipes-b3714',
+  storageBucket: 'gotorecipes-b3714.appspot.com',
+  messagingSenderId: '715471095704'
+}
 
 // react-redux-firebase config
 const rrfConfig = {
-    userProfile: 'users',
-    useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
-  }
+  userProfile: 'users',
+  useFirestoreForProfile: true // Firestore for Profile instead of Realtime DB
+}
 
 // Initialize firebase instance
 firebase.initializeApp(firebaseConfig)
@@ -31,17 +31,17 @@ const createStoreWithFirebase = compose(
 
 // Add firebase to reducers
 const rootReducer = combineReducers({
-    firebase: firebaseReducer,
-    firestore: firestoreReducer // <- needed if using firestore
-  })
+  firebase: firebaseReducer,
+  firestore: firestoreReducer // <- needed if using firestore
+})
 
 //  Initial state
 const initialState = {}
 
 // Store with reducers
 const store = createStoreWithFirebase(rootReducer, initialState, compose(
-    reactReduxFirebase(firebase),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reactReduxFirebase(firebase),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 export default store
