9717378eb585cd2259315d5eb0951fae07dbc484
Blake Fletcher <blkfltchr@gmail.com>
2018-12-19 18:19:08 -0500

    Dec19/Removed second navbar from Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import NavBar from './NavBar'
 import JumboTron from './JumboTron'
 
 const Home = () => {
   return (
     <div>
-      <NavBar />
       <JumboTron />
     </div>
   )
