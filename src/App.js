398692da70c478fddfb69076f7eb0d3fe1294733
Eddy <edward.gonzalez9708@gmail.com>
2018-12-21 11:04:29 -0500

    Moved React-Redux Provider and Router components to index.js.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import NavBar from './components/Home/NavBar'
 import Home from './components/Home/Home'
 import Recipes from './components/Recipe/Recipes'
 import Search from './components/Search/Search'
 import SingleRecipe from './components/Recipe/SingleRecipe'
 import AddRecipe from './components/Recipe/AddRecipe'
-import { Provider } from 'react-redux'
-import store from './store'
 
 class App extends Component {
   render () {
     return (
-      <Provider store={store}>
-        <Router>
-          <div className='App'>
-            <NavBar />
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/recipes' component={Recipes} />
-              <Route exact path='/search' component={Search} />
-              <Route exact path='/recipes/add' component={AddRecipe} />
-              <Route exact path='/recipes/healthy-egg' component={SingleRecipe} />
-            </Switch>
-          </div>
-        </Router>
-      </Provider>
+      <div className='App'>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/recipes' component={Recipes} />
+          <Route exact path='/search' component={Search} />
+          <Route exact path='/recipes/add' component={AddRecipe} />
+          <Route exact path='/recipes/:id' component={SingleRecipe} />
+        </Switch>
+      </div>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './App'
 
 // Setup react-redux so that connect HOC can be used
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>, document.getElementById('root'))
