2cfb3080282e9bc6bb9524d79a23ec91b8176ac4
Blake Fletcher <blkfltchr@gmail.com>
2019-01-22 10:23:12 -0500

    Fixed navbar styling for auth and nonauth

--- src/components/NavigationAuth/index.js
+++ src/components/NavigationAuth/index.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-
-import SignOut from '../SignOut'
 
 /* Route strings imported */
 
@@ -16,46 +13,27 @@ const {
 } = ROUTES
 
 const NavigationAuth = () => (
-  <nav className='navbar navbar-default'>
-    <div className='navbar-header'>
-      <button
-        className='navbar-toggle'
-        type='button'
-        data-target='#navbarCollapse'
-        data-toggle='collapse'>
-        <span className='sr-only'>Toggle navigation</span>
-        <span className='icon-bar' />
-        <span className='icon-bar' />
-        <span className='icon-bar' />
-      </button>
-      <Link
-        className='navbar-brand'
-        to={LANDING}>
-        Go To Recipes
-      </Link>
+  <div className='navbar navbar-expand-sm navbar-light bg-white'>
+    <a className='navbar-brand' href={LANDING}>GoToRecipes</a>
+    <button
+      className='navbar-toggler'
+      type='button'
+      data-toggle='collapse'
+      data-target='#navbarNavAltMarkup'
+      aria-controls='navbarNavAltMarkup'
+      aria-expanded='false'
+      aria-label='Toggle navigation'>
+      <span className='navbar-toggler-icon' />
+    </button>
+    <div className='collapse navbar-collapse justify-content-end' id='navbarNavAltMarkup'>
+      <div className='navbar nav-pills'>
+        <a className='nav-item nav-link' href={RECIPES}>My recipes</a>
+        <a className='nav-item nav-link' href={ADD_RECIPE}>Add a recipe</a>
+        <a className='nav-item nav-link' href={SEARCH}><i className='fas fa-search' /></a>
+        <a className='nav-item nav-link' href={ACCOUNT}><i className='fas fa-cog' /></a>
+      </div>
     </div>
-    <div id='navbarCollapse' className='collapse navbar-collapse'>
-      <ul className='nav navbar-nav'>
-        <li className='active'>
-          <Link to={RECIPES}>My Recipes</Link>
-        </li>
-        <li>
-          <Link to={ADD_RECIPE}>Add a Recipe</Link>
-        </li>
-        <li>
-          <Link to={SEARCH}>Search for Recipes</Link>
-        </li>
-        <li>
-          <Link to={ACCOUNT}>Account</Link>
-        </li>
-      </ul>
-      <ul className='nav navbar-nav navbar-right'>
-        <li>
-          <SignOut />
-        </li>
-      </ul>
-    </div>
-  </nav>
+  </div>
 )
 
 export default NavigationAuth
