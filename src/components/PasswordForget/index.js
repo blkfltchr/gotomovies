4411bd8e3b1377ec0770bf8910bf6a09e4fe500b
Blake Fletcher <blkfltchr@gmail.com>
2019-02-01 17:24:18 -0500

    Changed forget password styling

--- src/components/PasswordForget/index.js
+++ src/components/PasswordForget/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import './index.css'
 import PasswordForgetForm from '../PasswordForgetForm'
 
 const PasswordForget = () => (
-  <div className='container mt-2'>
-    <h1>Forgot your password?</h1>
-    <PasswordForgetForm />
+  <div className='jumbo-background d-flex justify-content-end align-items-center'>
+    <div className='container mt-2 forget-box'>
+      <h1>Oops, did you forget your password?</h1>
+      <PasswordForgetForm />
+    </div>
   </div>
 )
 
